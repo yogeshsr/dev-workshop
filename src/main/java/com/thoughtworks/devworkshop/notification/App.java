6fb6bf40c8667e4ebfb332210e88f279bd8e6471
Yogesh Sreedharan <yogesh.s.aux@go-jek.com>
2021-11-26 22:44:13 +0530

    added App for notification

---
+++ src/main/java/com/thoughtworks/devworkshop/notification/App.java
@@ -0,0 +1,8 @@
+package com.thoughtworks.devworkshop.notification;
+
+public class App {
+  public static void main(String[] args) {
+    Notification notification = new Notification();
+    notification.send();
+  }
+}
